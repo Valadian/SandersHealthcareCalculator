a938a49e3e486f9d03a7192150c6b140bb73736c
Valadian <berger_jesse@yahoo.com>
2016-01-18 17:08:15 -0600

    Architecture solid, tax calculations almost complete

---
+++ Scripts/site.js
@@ -0,0 +1,216 @@
+function clamp(num, min, max) {
+  return num < min ? min : num > max && max!=null ? max : num;
+}
+
+$(function(){
+	$('form').validate({
+        rules: {},
+        highlight: function(element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+            if(element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
+	jQuery.validator.addClassRules("currency",{
+		number: true,
+		min: 0
+	});
+	jQuery.validator.addClassRules("naturalNumber",{
+		digits: true,
+		min: 0
+	});
+	
+	$('#deductiblePercentage').slider({
+		formatter: function(value) {
+			return (value * 100).toFixed(0) + "%";
+		}
+	});
+	$('.advanced').hide();
+	$('.advanced-toggle').click(function(event){
+		var hidden = $(this).children('.glyphicon-chevron-down').is(':visible');
+		$(this).children('.glyphicon-chevron-down').toggle();
+		$(this).children('.glyphicon-chevron-up').toggle();
+		//var children = $(this).prev().children('.advanced')
+		var children = $(this).closest('.panel').find('.advanced');
+		if(hidden){
+			children.slideDown(400);
+		}
+		else{
+			children.slideUp(400);
+		}
+		 event.stopPropagation();
+	});
+	$('.currency').after("<div class='input-group-addon currency'>$</div>");
+	var taxTable = [];
+	
+	var jsModel = { 
+		income: 50000, 
+		incomeShortCapGains: 0, 
+		incomeLongCapGains: 0, 
+		filingStatus:1,
+		selfEmployed: 0,
+		familySize: 4,
+		itemizedDeductions: 0,
+		insured: 1,
+		premium: 4955,
+		premiumPeriod: 1,
+		premiumEmployer: 12591,
+		premiumEmployerPeriod: 1,
+		copays: 500,
+		copaysPeriod: 1,
+		deductible: 1318,
+		deductiblePercentage: 1,
+	};
+	var self = ko.mapping.fromJS(jsModel);
+	var standardDeductionTable = [6300,12600,6300,9250]
+	self.standardDeduction = ko.pureComputed(function(){
+	    return standardDeductionTable[self.filingStatus()];
+	},self);
+
+	self.exemptions = ko.pureComputed(function(){
+        return 4000*self.familySize();
+    },self);
+
+	self.totalDeductions = ko.pureComputed(function(){
+	    return Math.max(self.standardDeduction(),self.itemizedDeductions())+ +self.exemptions();
+	},self);
+
+	self.taxableIncome = ko.pureComputed(function(){
+	    return +self.income() + +self.incomeShortCapGains() - +self.totalDeductions();
+	},self);
+
+	var taxTable2016 = [
+	    [//Single
+	        {start: 0,      stop:9275,      rate:0.10,  capGainsRate: 0},
+	        {start: 9275,   stop:37650,     rate:0.15,  capGainsRate: 0},
+            {start: 37650,  stop:91150,     rate:0.25,  capGainsRate: 0.15},
+            {start: 91150,  stop:190150,    rate:0.28,  capGainsRate: 0.15},
+            {start: 190150, stop:413350,    rate:0.33,  capGainsRate: 0.15},
+            {start: 413350, stop:415050,    rate:0.35,  capGainsRate: 0.15},
+            {start: 415050, stop:null,      rate:0.396, capGainsRate: 0.20}
+	    ],
+	    [//Married Filing Jointly
+	        {start: 0,      stop:18550,     rate:0.10,  capGainsRate: 0},
+	        {start: 18550,  stop:75300,     rate:0.15,  capGainsRate: 0},
+            {start: 75300,  stop:151900,    rate:0.25,  capGainsRate: 0.15},
+            {start: 151900, stop:231450,    rate:0.28,  capGainsRate: 0.15},
+            {start: 231450, stop:413350,    rate:0.33,  capGainsRate: 0.15},
+            {start: 413350, stop:466950,    rate:0.35,  capGainsRate: 0.15},
+            {start: 466950, stop:null,      rate:0.396, capGainsRate: 0.20}
+	    ],
+        [//Married Filing Separately
+            {start: 0,      stop:9275,      rate:0.10,  capGainsRate: 0},
+            {start: 9275,   stop:37650,     rate:0.15,  capGainsRate: 0},
+            {start: 37650,  stop:75950,     rate:0.25,  capGainsRate: 0.15},
+            {start: 75950,  stop:115725,    rate:0.28,  capGainsRate: 0.15},
+            {start: 115725, stop:206675,    rate:0.33,  capGainsRate: 0.15},
+            {start: 206675, stop:233475,    rate:0.35,  capGainsRate: 0.15},
+            {start: 233475, stop:null,      rate:0.396, capGainsRate: 0.20}
+        ],
+        [//Head of Household
+            {start: 0,      stop:13250,     rate:0.10,  capGainsRate: 0},
+            {start: 13250,  stop:50400,     rate:0.15,  capGainsRate: 0},
+            {start: 50400,  stop:130150,    rate:0.25,  capGainsRate: 0.15},
+            {start: 130150, stop:210800,    rate:0.28,  capGainsRate: 0.15},
+            {start: 210800, stop:413350,    rate:0.33,  capGainsRate: 0.15},
+            {start: 413350, stop:441000,    rate:0.35,  capGainsRate: 0.15},
+            {start: 441000, stop:null,      rate:0.396, capGainsRate: 0.20}
+        ],
+        [//Qualified Widow(er)
+            {start: 0,      stop:18550,     rate:0.10,  capGainsRate: 0},
+            {start: 18550,  stop:75300,     rate:0.15,  capGainsRate: 0},
+            {start: 75300,  stop:151900,    rate:0.25,  capGainsRate: 0.15},
+            {start: 151900, stop:231450,    rate:0.28,  capGainsRate: 0.15},
+            {start: 231450, stop:413350,    rate:0.33,  capGainsRate: 0.15},
+            {start: 413350, stop:466950,    rate:0.35,  capGainsRate: 0.15},
+            {start: 466950, stop:null,      rate:0.396, capGainsRate: 0.20}
+        ]
+	];
+	self.federalWithholding = ko.pureComputed(function(){
+        return taxTable2016[self.filingStatus()].reduce(function(curr,next,i,array){
+            if(!("sum" in curr)){
+                curr = {sum: (clamp(self.taxableIncome(),curr.start,curr.stop) - curr.start) * curr.rate};
+            }
+            return {sum: curr.sum + (clamp(self.taxableIncome(),next.start,next.stop) - next.start) * next.rate};
+        }).sum;
+    },self);
+
+	self.longCapGainsTax = ko.pureComputed(function(){
+	    var sum = 0
+        var taxed = 0
+        var array = taxTable2016[self.filingStatus()];
+        for(var i=0, n=array.length; i< n; i++){
+            curr = array[i];
+            var clampedIncome = clamp(self.taxableIncome(),curr.start,curr.stop);
+            var clampedLongGains = clamp(self.taxableIncome()+ +self.incomeLongCapGains(),curr.start,curr.stop);
+            var taxableLongGains = Math.min(clampedLongGains - +clampedIncome,self.incomeLongCapGains()-taxed);
+            sum = sum + +taxableLongGains * curr.capGainsRate;
+            taxed = taxed + +taxableLongGains;
+        }
+        return sum;
+    },self);
+
+    self.employeeMediTax = ko.pureComputed(function(){
+        if(+self.selfEmployed()){
+            return (0.9235 * self.income() * 0.0145 * 2).toFixed(0);
+        }
+        else{
+            return (self.income() * 0.0145).toFixed(0);
+        }
+    },self);
+
+    self.employerMediTax = ko.pureComputed(function(){
+        if(+self.selfEmployed()){
+            return 0;
+        } else {
+            return (self.income() * 0.0145).toFixed(0);
+        }
+    },self);
+
+    self.employeeSocSecTax = ko.pureComputed(function(){
+        if(+self.selfEmployed()){
+            return (0.9235 * self.income() * 0.062 * 2).toFixed(0);
+        }
+        else{
+            return (self.income() * 0.062).toFixed(0);
+        }
+    },self);
+
+    self.employerSocSecTax = ko.pureComputed(function(){
+        if(+self.selfEmployed()){
+            return 0;
+        } else {
+            return (self.income() * 0.062).toFixed(0);
+        }
+    },self);
+
+	ko.bindingHandlers.tooltip = {
+        init: function(element, valueAccessor) {
+            var local = ko.utils.unwrapObservable(valueAccessor()),
+                options = {};
+
+            ko.utils.extend(options, ko.bindingHandlers.tooltip.options);
+            ko.utils.extend(options, local);
+
+            $(element).tooltip(options);
+
+            ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+                $(element).tooltip("destroy");
+            });
+        },
+        options: {
+            trigger: "click"
+        }
+    };
+	ko.applyBindings(self);
+});
